2feeb98e497101c008b99df1cd346d0d393038b8
JIten Patel <jiten@sipup.co.uk>
2026-06-16 00:36:20 +0100

    updated seeder

--- database/seeders/DefaultCompanySeeder.php
+++ database/seeders/DefaultCompanySeeder.php
@@ -12,10 +12,12 @@ class DefaultCompanySeeder extends Seeder
     public function run(): void
     {
         Company::query()->updateOrCreate(
-            ['name' => 'Default Perfume Shop'],
+            [
+                'name' => 'Default Perfume Shop',
+                'email' => 'admin@seo.com'
+            ],
             [
                 'contact_person_name' => 'Admin',
-                'email' => 'admin@seo.com',
                 'phone' => '00000000000',
                 'legal_business_name' => 'Default Perfume Shop',
                 'business_phone_number' => '00000000000',
