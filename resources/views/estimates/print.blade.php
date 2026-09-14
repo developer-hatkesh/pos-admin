eccac31182ddb43aed33e3c663b3502b98b83074
developer-hatkesh <developer-hatkesh@users.noreply.github.com>
2026-09-14 10:53:32 +0100

    Hide product codes on estimate prints

--- resources/views/estimates/print.blade.php
+++ resources/views/estimates/print.blade.php
@@ -73,7 +73,6 @@
     $showAddress = $setting('receipt_show_address');
     $showEmail = $setting('receipt_show_email');
     $showDiscount = $setting('receipt_show_discount_shipping');
-    $showProductCode = $setting('receipt_show_product_code');
     $showTax = $setting('receipt_show_tax');
     $fontCss = fn (string $style): string => match ($style) { 'bold' => 'font-weight: 700;', 'italic' => 'font-style: italic;', default => 'font-weight: 400;' };
     $labelFontCss = $fontCss((string) ($receiptSettings['receipt_labels_font_style'] ?? 'bold'));
@@ -112,7 +111,7 @@
             $lineTotal = (float) ($computedLine['line_total'] ?? $item->line_total) - $lineDiscount;
             $product = $item->productItem;
         @endphp
-        <tr><td>{{ $loop->iteration }}</td><td><strong>{{ $product?->name ?: 'Item' }}</strong>@if(filled($item->description))<div class="item-meta">{{ $item->description }}</div>@endif @if($showProductCode && $product?->item_code)<div class="item-meta">Code: {{ $product->item_code }}</div>@endif @if($product?->variation?->name)<div class="item-meta">{{ $product->variation->name }}: {{ $product?->variationType?->name }}</div>@endif</td><td class="text-center">{{ rtrim(rtrim(number_format((float) $item->qty, 3), '0'), '.') }}</td><td class="text-center">{{ app_money((float) $item->rate) }}</td>@if($showDiscount)<td class="text-center">{{ app_money($lineDiscount) }}</td>@endif @if($showTax)<td class="text-center">{{ app_money($lineTax) }}</td>@endif<td class="text-center"><strong>{{ app_money($lineTotal) }}</strong></td></tr>
+        <tr><td>{{ $loop->iteration }}</td><td><strong>{{ $product?->name ?: 'Item' }}</strong>@if(filled($item->description))<div class="item-meta">{{ $item->description }}</div>@endif @if($product?->variation?->name)<div class="item-meta">{{ $product->variation->name }}: {{ $product?->variationType?->name }}</div>@endif</td><td class="text-center">{{ rtrim(rtrim(number_format((float) $item->qty, 3), '0'), '.') }}</td><td class="text-center">{{ app_money((float) $item->rate) }}</td>@if($showDiscount)<td class="text-center">{{ app_money($lineDiscount) }}</td>@endif @if($showTax)<td class="text-center">{{ app_money($lineTax) }}</td>@endif<td class="text-center"><strong>{{ app_money($lineTotal) }}</strong></td></tr>
     @empty <tr><td colspan="{{ 5 + (int) $showDiscount + (int) $showTax }}">No estimate items</td></tr> @endforelse
     </tbody></table>
     <section class="bottom">
